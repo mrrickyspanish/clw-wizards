d06956fc01e12062630708f19baefb65a18923c1
mrrickyspanish <rj@creativeeyemultimedia.com>
2026-07-08 20:17:29 +0000

    Route mobile join bar through new family guide

--- src/components/landing/MobileCtaBar.tsx
+++ src/components/landing/MobileCtaBar.tsx
@@ -9,7 +9,7 @@ export function MobileCtaBar() {
     <div className="fixed inset-x-0 bottom-0 z-[100] border-t border-clw-gold/10 bg-clw-black/95 px-4 pb-3 pt-3 backdrop-blur-md [backface-visibility:hidden] [transform:translateZ(0)] md:hidden">
       <div className="flex items-center gap-3">
         <Button asChild className="flex-1">
-          <Link href="/signup">Join the Wizards</Link>
+          <Link href="/join">Join the Wizards</Link>
         </Button>
         <Button asChild variant="outline">
           <Link href="/login">Sign in</Link>
