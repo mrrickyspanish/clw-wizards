83d7f986f7769ecbf87e2dffdb551a4a34d77936
mrrickyspanish <rj@creativeeyemultimedia.com>
2026-07-08 01:30:13 +0000

    Refine tape dividers into shallow overlapping section edges

--- src/components/landing/HomeMatTapeDivider.tsx
+++ src/components/landing/HomeMatTapeDivider.tsx
@@ -8,14 +8,19 @@ const TAPE_ASSETS: Record<TapeProfile, string> = {
 export function HomeMatTapeDivider({ profile }: { profile: TapeProfile }) {
   return (
     <div aria-hidden className="pointer-events-none relative z-30 h-0 w-full overflow-visible">
-      {/* eslint-disable-next-line @next/next/no-img-element -- supplied transparent wrestling tape artwork */}
-      <img
-        src={TAPE_ASSETS[profile]}
-        alt=""
-        draggable={false}
-        className="absolute left-1/2 top-0 h-auto w-[104vw] max-w-none select-none"
-        style={{ transform: 'translate(-50%, -54%)' }}
-      />
+      <div
+        className="absolute left-0 top-0 h-9 w-full overflow-hidden sm:h-10 lg:h-12"
+        style={{ transform: 'translateY(-72%)' }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element -- supplied transparent wrestling tape artwork */}
+        <img
+          src={TAPE_ASSETS[profile]}
+          alt=""
+          draggable={false}
+          className="absolute left-1/2 top-1/2 h-auto w-[104vw] max-w-none select-none"
+          style={{ transform: 'translate(-50%, -42%)' }}
+        />
+      </div>
     </div>
   )
 }
