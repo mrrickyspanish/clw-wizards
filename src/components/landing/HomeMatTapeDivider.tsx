72e44b15d8b114a6c1a5ab1bb67820fb3f2e0e31
mrrickyspanish <rj@creativeeyemultimedia.com>
2026-07-08 16:16:49 +0000

    Tune each tape profile for a shallower section edge

--- src/components/landing/HomeMatTapeDivider.tsx
+++ src/components/landing/HomeMatTapeDivider.tsx
@@ -1,24 +1,43 @@
 type TapeProfile = 'white-left' | 'black-right'
 
-const TAPE_ASSETS: Record<TapeProfile, string> = {
-  'white-left': '/images/real/clw_tape_white_profile_a_star_left.png',
-  'black-right': '/images/real/clw_tape_black_profile_b_star_right.png',
+type TapeSettings = {
+  src: string
+  frameClassName: string
+  frameOffset: string
+  imageOffset: string
+}
+
+const TAPE_SETTINGS: Record<TapeProfile, TapeSettings> = {
+  'white-left': {
+    src: '/images/real/clw_tape_white_profile_a_star_left.png',
+    frameClassName: 'h-6 sm:h-7 lg:h-8',
+    frameOffset: 'translateY(-68%)',
+    imageOffset: 'translate(-50%, -43%)',
+  },
+  'black-right': {
+    src: '/images/real/clw_tape_black_profile_b_star_right.png',
+    frameClassName: 'h-7 sm:h-8 lg:h-9',
+    frameOffset: 'translateY(-66%)',
+    imageOffset: 'translate(-50%, -39%)',
+  },
 }
 
 export function HomeMatTapeDivider({ profile }: { profile: TapeProfile }) {
+  const tape = TAPE_SETTINGS[profile]
+
   return (
     <div aria-hidden className="pointer-events-none relative z-30 h-0 w-full overflow-visible">
       <div
-        className="absolute left-0 top-0 h-9 w-full overflow-hidden sm:h-10 lg:h-12"
-        style={{ transform: 'translateY(-72%)' }}
+        className={`absolute left-0 top-0 w-full overflow-hidden ${tape.frameClassName}`}
+        style={{ transform: tape.frameOffset }}
       >
         {/* eslint-disable-next-line @next/next/no-img-element -- supplied transparent wrestling tape artwork */}
         <img
-          src={TAPE_ASSETS[profile]}
+          src={tape.src}
           alt=""
           draggable={false}
           className="absolute left-1/2 top-1/2 h-auto w-[104vw] max-w-none select-none"
-          style={{ transform: 'translate(-50%, -42%)' }}
+          style={{ transform: tape.imageOffset }}
         />
       </div>
     </div>
