4d217586595dee9953f3a73f05621616bbef29fd
mrrickyspanish <rj@creativeeyemultimedia.com>
2026-07-27 18:03:05 -0500

    Match marketing page offset to the tightened header

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -5,7 +5,7 @@ export default function MarketingLayout({ children }: { children: React.ReactNod
   return (
     <div className="marketing-site min-h-screen bg-clw-black">
       <SiteHeader />
-      <div className="pt-[142px] sm:pt-[144px] lg:pt-[148px]">{children}</div>
+      <div className="pt-[124px] sm:pt-[128px] lg:pt-[148px]">{children}</div>
       <SiteFooter />
     </div>
   )
