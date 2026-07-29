7c3aea51a991111ab93c1d84b97d85da21f2a455
mrrickyspanish <rj@creativeeyemultimedia.com>
2026-07-29 15:54:33 -0500

    Label season registration on public calendar

--- src/components/events/EventsList.tsx
+++ src/components/events/EventsList.tsx
@@ -20,6 +20,7 @@ const KIND_LABELS: Record<CalendarItem['kind'], string> = {
   parent_night: 'Parent Night',
   fundraiser: 'Fundraiser',
   meeting: 'Meeting',
+  season_registration: 'Season Registration',
   event: 'Club Event',
   other: 'Club Event',
 }
