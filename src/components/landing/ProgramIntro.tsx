65c997c0a33df40a929c27d9c10b6365e145ac12
mrrickyspanish <rj@creativeeyemultimedia.com>
2026-07-29 23:39:21 -0500

    Reposition homepage intro photo crop

--- src/components/landing/ProgramIntro.tsx
+++ src/components/landing/ProgramIntro.tsx
@@ -43,7 +43,7 @@ export async function ProgramIntro() {
                 <img
                   src="/images/real/clw-wizards-trio-featured-photo.jpg"
                   alt="Three young Wizards wrestlers in club gear at an IKWF state-series tournament"
-                  className="h-full w-full object-cover object-center contrast-105"
+                  className="h-full w-full object-cover object-center contrast-105 lg:object-[center_20%]"
                 />
                 <div aria-hidden className="pointer-events-none absolute inset-0 [box-shadow:inset_0_0_0_1px_rgba(17,17,17,0.14)]" />
               </div>
